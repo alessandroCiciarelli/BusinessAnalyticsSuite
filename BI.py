e06ced37e764bc08287eb22a082b0aeddefbf67c
alessandroCiciarelli <36853940+alessandroCiciarelli@users.noreply.github.com>
2021-11-13 15:40:39 +0100

    Add files via upload

--- BI.py
+++ BI.py
@@ -13,6 +13,7 @@ import base64
 import pyrebase
 import tpot
 from tabula import read_pdf
+import camelot
 import html5lib
 import requests
 import time
@@ -367,14 +368,18 @@ def pdftocsv():
 		if st.button("Fammi vedere Che riesci a fare.. - Costo 1 credito"):
 			rimuoviCredito()
 			try:
-				df = read_pdf(uploaded_file, pages='all')[0]
-				df = df.dropna()
-				st.write(df)
-				csv = df.to_csv(index=False)
-				b64 = base64.b64encode(csv.encode()).decode()
-				st.markdown('### ** ⬇️ Scarica la tabella in formato csv **')
-				href = f'<a href="data:file/csv;base64,{b64}" download="PDF_table.csv">** Clicca Qui per Scaricare il Tuo Dataset! 🎉**</a>'
-				st.markdown(href, unsafe_allow_html=True)
+				#df = read_pdf(uploaded_file, pages='all')[0]
+				tables = camelot.read_pdf('foo.pdf')
+				for tabelle in tables :
+					with st.expander("Tabella numer : " + str(tabelle)):
+						df_temp = tabelle.df
+						df_temp = df_temp.dropna()
+						st.write(df_temp)
+						csv = df_temp.to_csv(index=False)
+						b64 = base64.b64encode(csv.encode()).decode()
+						st.markdown('### ** ⬇️ Scarica la tabella in formato csv **')
+						href = f'<a href="data:file/csv;base64,{b64}" download="PDF_table_{str(tabelle)}.csv">** Clicca Qui per Scaricare il Tuo Dataset! 🎉**</a>'
+						st.markdown(href, unsafe_allow_html=True)
 			except ValueError:
 				st.info ("ℹ️ - Non abbiamo trovato tabelle da Esportare ! 😊")
 
