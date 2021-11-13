752f8140620610ad4b0c122c89a726ffc7bcc155
alessandroCiciarelli <36853940+alessandroCiciarelli@users.noreply.github.com>
2021-11-13 17:02:55 +0100

    Add files via upload

--- BI.py
+++ BI.py
@@ -85,8 +85,15 @@ def app(dataset):
 
 
 def rimuoviCredito():
+	st.session_state.count = db.child(st.session_state.id).child("Crediti").get().val()
 	st.session_state.count = st.session_state.count - 1
 	db.child(st.session_state.id).update({"Crediti":st.session_state.count})
+	st.session_state.count = db.child(st.session_state.id).child("Crediti").get().val()
+
+def finecredito()
+	cr = db.child(st.session_state.id).child("Crediti").get().val()
+	if cr >= 0 : return true
+	else return false
 
 
 ############################################ANALYTIC SUITE
@@ -123,12 +130,16 @@ def AnalyticSuite()  :
 		                         
 	    if task == "Crea Report Personalizzato":
 	    	if(st.button("Genera 2 Report - Costo 1 credito")):
-	    		rimuoviCredito()
-		    	pr = ProfileReport(dataset, explorative=True, orange_mode=False)
-		    	st_profile_report(pr)
-		    	pr.to_file("EDA.html")
-		    	st.markdown(get_binary_file_downloader_html('EDA.html', 'Report'), unsafe_allow_html=True)
-		    	app(dataset)
+	    		if finecredito() :
+		    		rimuoviCredito()
+			    	pr = ProfileReport(dataset, explorative=True, orange_mode=False)
+			    	st_profile_report(pr)
+			    	pr.to_file("EDA.html")
+			    	st.markdown(get_binary_file_downloader_html('EDA.html', 'Report'), unsafe_allow_html=True)
+			    	app(dataset)
+			else:
+				st.error('Attenzione hai finito i crediti')
+
 	    	
 	    elif task == "Scopri il Miglior Algoritmo di ML per i tuoi dati":
 	    	datasetMalgo = dataset
@@ -153,33 +164,39 @@ def AnalyticSuite()  :
 	    	X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=percentuale_dati_test)
 		
 	    	if(st.button("Svelami il Miglior Algoritmo per i miei dati- Costo 1 credito")):
-	    		rimuoviCredito()
-	    		if(tipo_di_problema == "CLASSIFICAZIONE"):
-	    			from lazypredict.Supervised import LazyClassifier
-	    			clf = LazyClassifier(verbose=0,ignore_warnings=True, custom_metric=None)
-	    			models,predictions = clf.fit(X_train, X_test, y_train, y_test)
-	    			st.write(models)
-	    			models = pd.DataFrame(models)
-	    			models.to_csv("model.csv")
-	    			st.markdown(get_binary_file_downloader_html('model.csv', 'Rapporto Modelli Predittivi'), unsafe_allow_html=True)
-	    		if(tipo_di_problema == "REGRESSIONE"):
-	    			from lazypredict.Supervised import LazyRegressor
-	    			reg = LazyRegressor(verbose=0, ignore_warnings=True, custom_metric=None)
-	    			models, predictions = reg.fit(X_train, X_test, y_train, y_test)
-	    			st.write(models)
-	    			models = pd.DataFrame(models)
-	    			models.to_csv("model.csv")
-	    			st.markdown(get_binary_file_downloader_html('model.csv', 'Rapporto Modelli Predittivi'), unsafe_allow_html=True)
+	    		if finecredito() :
+	    			rimuoviCredito()
+		    		if(tipo_di_problema == "CLASSIFICAZIONE"):
+		    			from lazypredict.Supervised import LazyClassifier
+		    			clf = LazyClassifier(verbose=0,ignore_warnings=True, custom_metric=None)
+		    			models,predictions = clf.fit(X_train, X_test, y_train, y_test)
+		    			st.write(models)
+		    			models = pd.DataFrame(models)
+		    			models.to_csv("model.csv")
+		    			st.markdown(get_binary_file_downloader_html('model.csv', 'Rapporto Modelli Predittivi'), unsafe_allow_html=True)
+		    		if(tipo_di_problema == "REGRESSIONE"):
+		    			from lazypredict.Supervised import LazyRegressor
+		    			reg = LazyRegressor(verbose=0, ignore_warnings=True, custom_metric=None)
+		    			models, predictions = reg.fit(X_train, X_test, y_train, y_test)
+		    			st.write(models)
+		    			models = pd.DataFrame(models)
+		    			models.to_csv("model.csv")
+		    			st.markdown(get_binary_file_downloader_html('model.csv', 'Rapporto Modelli Predittivi'), unsafe_allow_html=True)
+		    	else:
+				st.error('Attenzione hai finito i crediti')
 	    			
 	    elif task == "Utilizza le Query SQL sui tuoi dati":
 	    	q = st.text_input("Scrivi qui dentro la tua Query", value="SELECT * FROM dataset")
 	    	if st.button("Applica Query SQL - Costo 1 credito"):
-	    		rimuoviCredito()
-	    		df = sqldf(q)
-	    		df = pd.DataFrame(df)
-	    		st.write(df)
-	    		df.to_csv("Dataset_query.csv")
-	    		st.markdown(get_binary_file_downloader_html('Dataset_query.csv', 'Riusltato qyery Sql IAITALIA'), unsafe_allow_html=True)
+	    		if finecredito() :
+		    		rimuoviCredito()
+		    		df = sqldf(q)
+		    		df = pd.DataFrame(df)
+		    		st.write(df)
+		    		df.to_csv("Dataset_query.csv")
+		    		st.markdown(get_binary_file_downloader_html('Dataset_query.csv', 'Riusltato qyery Sql IAITALIA'), unsafe_allow_html=True)
+		    	else:
+				st.error('Attenzione hai finito i crediti')
 	    
 	    elif task == "Crea PipeLine ADHOC in Python per i tuoi dati":
 	    	datasetPalgo = dataset
@@ -210,37 +227,40 @@ def AnalyticSuite()  :
 	    	X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=percentuale_dati_test)
 		
 	    	if(st.button("Creami la miglior pipeline in Python perfavore - Costo 1 credito")):
-	    		rimuoviCredito()
-		    	if tipo=="CLASSIFICAZIONE":
-		    		from tpot import TPOTClassifier
-		    		pipeline_optimizer = TPOTClassifier()
-		    		pipeline_optimizer = TPOTClassifier(generations=gen, population_size=pop, scoring=sel_scor, cv=5,
-				                    random_state=42, verbosity=2)
-		    		pipeline_optimizer.fit(X_train, y_train)
-		    		st.write(f"Accuratezza PIPELINE : {pipeline_optimizer.score(X_test, y_test)*100} %")
-		    		pipeline_optimizer.export('IAITALIA_exported_pipeline.py')
-		    		filepipeline = open("IAITALIA_exported_pipeline.py", 'r', encoding='utf-8')
-		    		source_code = filepipeline.read() 
-		    		st.subheader("Miglior PipeLine Rilevata Sui tuoi Dati ")
-		    		my_text = st.text_area(label="Hai visto, Scriviamo anche il codice al posto tuo...", value=source_code, height=500)
+	    		if finecredito() :
+		    		rimuoviCredito()
+			    	if tipo=="CLASSIFICAZIONE":
+			    		from tpot import TPOTClassifier
+			    		pipeline_optimizer = TPOTClassifier()
+			    		pipeline_optimizer = TPOTClassifier(generations=gen, population_size=pop, scoring=sel_scor, cv=5,
+						            random_state=42, verbosity=2)
+			    		pipeline_optimizer.fit(X_train, y_train)
+			    		st.write(f"Accuratezza PIPELINE : {pipeline_optimizer.score(X_test, y_test)*100} %")
+			    		pipeline_optimizer.export('IAITALIA_exported_pipeline.py')
+			    		filepipeline = open("IAITALIA_exported_pipeline.py", 'r', encoding='utf-8')
+			    		source_code = filepipeline.read() 
+			    		st.subheader("Miglior PipeLine Rilevata Sui tuoi Dati ")
+			    		my_text = st.text_area(label="Hai visto, Scriviamo anche il codice al posto tuo...", value=source_code, height=500)
 
-		    		st.markdown(get_binary_file_downloader_html('IAITALIA_exported_pipeline.py', 'pipeline.py IAITALIA'), unsafe_allow_html=True)
-		    		
-		    	if tipo=="REGRESSIONE":
-		    		from tpot import TPOTRegresso
-		    		pipeline_optimizer = TPOTRegressor()
-		    		pipeline_optimizer = TPOTRegressor(generations=gen, population_size=pop, scoring=sel_scor, cv=5,
-				                    random_state=42, verbosity=2)
-		    		pipeline_optimizer.fit(X_train, y_train)
-		    		st.write(f"Accuratezza PIPELINE : {pipeline_optimizer.score(X_test, y_test)*100} %")
-		    		pipeline_optimizer.export('IAITALIA_exported_pipeline.py')
-		    		filepipeline = open("IAITALIA_exported_pipeline.py", 'r', encoding='utf-8')
-		    		source_code = filepipeline.read() 
-		    		st.subheader("Miglior PipeLine Rilevata Sui tuoi Dati ")
-		    		my_text = st.text_area(label="Hai visto, Scriviamo anche il codice al posto tuo...", value=source_code, height=500)
+			    		st.markdown(get_binary_file_downloader_html('IAITALIA_exported_pipeline.py', 'pipeline.py IAITALIA'), unsafe_allow_html=True)
+			    		
+			    	if tipo=="REGRESSIONE":
+			    		from tpot import TPOTRegresso
+			    		pipeline_optimizer = TPOTRegressor()
+			    		pipeline_optimizer = TPOTRegressor(generations=gen, population_size=pop, scoring=sel_scor, cv=5,
+						            random_state=42, verbosity=2)
+			    		pipeline_optimizer.fit(X_train, y_train)
+			    		st.write(f"Accuratezza PIPELINE : {pipeline_optimizer.score(X_test, y_test)*100} %")
+			    		pipeline_optimizer.export('IAITALIA_exported_pipeline.py')
+			    		filepipeline = open("IAITALIA_exported_pipeline.py", 'r', encoding='utf-8')
+			    		source_code = filepipeline.read() 
+			    		st.subheader("Miglior PipeLine Rilevata Sui tuoi Dati ")
+			    		my_text = st.text_area(label="Hai visto, Scriviamo anche il codice al posto tuo...", value=source_code, height=500)
 
-		    		st.markdown(get_binary_file_downloader_html('IAITALIA_exported_pipeline.py', 'pipeline.py IAITALIA'), unsafe_allow_html=True)
-		    	
+			    		st.markdown(get_binary_file_downloader_html('IAITALIA_exported_pipeline.py', 'pipeline.py IAITALIA'), unsafe_allow_html=True)
+			    
+	    		else:
+				st.error('Attenzione hai finito i crediti')
 		    	
 		    	
 	    elif task == "Pulisci i Miei Dati":
@@ -257,19 +277,22 @@ def AnalyticSuite()  :
 	    	source_code = fileinfo.read() 
 	    	st.text_area(label="info...", value=source_code, height=300)
 	    	if( st.button("Pulisci i miei dati - Costo 1 credito")):
-	    	    	rimuoviCredito()
-	    	    	st.subheader("Ecco qualche INFO sul tuo Dataset Dopo essere stato Pulito")
-	    	    	dataset_pulito=autoclean(dataset)
-	    	    	buffer = io.StringIO() 
-	    	    	dataset.info(buf=buffer)
-	    	    	s = buffer.getvalue() 
-	    	    	with open("df_info.txt", "w", encoding="utf-8") as f:
-	    	    	     f.write(s) 
-	    	    	fileinfo = open("df_info.txt", 'r', encoding='utf-8')
-	    	    	source_code = fileinfo.read() 
-	    	    	st.text_area(label="info dati puliti...", value=source_code, height=300)
-	    	    	dataset_pulito.to_csv('I_tuoi_dati_puliti_by_IAITALIA.csv', sep=',', index=False)
-	    	    	st.markdown(get_binary_file_downloader_html('I_tuoi_dati_puliti_by_IAITALIA.csv', 'Dati puliti by IAITALIA'), unsafe_allow_html=True)
+	    		if finecredito() :
+		    	    	rimuoviCredito()
+		    	    	st.subheader("Ecco qualche INFO sul tuo Dataset Dopo essere stato Pulito")
+		    	    	dataset_pulito=autoclean(dataset)
+		    	    	buffer = io.StringIO() 
+		    	    	dataset.info(buf=buffer)
+		    	    	s = buffer.getvalue() 
+		    	    	with open("df_info.txt", "w", encoding="utf-8") as f:
+		    	    	     f.write(s) 
+		    	    	fileinfo = open("df_info.txt", 'r', encoding='utf-8')
+		    	    	source_code = fileinfo.read() 
+		    	    	st.text_area(label="info dati puliti...", value=source_code, height=300)
+		    	    	dataset_pulito.to_csv('I_tuoi_dati_puliti_by_IAITALIA.csv', sep=',', index=False)
+		    	    	st.markdown(get_binary_file_downloader_html('I_tuoi_dati_puliti_by_IAITALIA.csv', 'Dati puliti by IAITALIA'), unsafe_allow_html=True)
+	    		else:
+				st.error('Attenzione hai finito i crediti')
 
 
 
@@ -291,62 +314,65 @@ def ScrapeSuite():
 	try:
 	    url =  st.text_input("", value='https://www.tuttosport.com/live/classifica-serie-a', max_chars=None, key=None, type='default')
 	    if url and st.button("Cerca le tabelle nella pagina "):
-    		rimuoviCredito()
-	    	arr = ['https://', 'http://']
-	    	if any(c in url for c in arr):
-	    	    header = {
-		    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
-		    "X-Requested-With": "XMLHttpRequest"
-		    }
+	    	if finecredito() :
+	    		rimuoviCredito()
+		    	arr = ['https://', 'http://']
+		    	if any(c in url for c in arr):
+		    	    header = {
+			    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+			    "X-Requested-With": "XMLHttpRequest"
+			    }
 
-	    	    @st.cache(persist=True, show_spinner=False)
-	    	    def load_data():
-	    	        r = requests.get(url, headers=header)
-	    	        return pd.read_html(r.text)
+		    	    @st.cache(persist=True, show_spinner=False)
+		    	    def load_data():
+		    	        r = requests.get(url, headers=header)
+		    	        return pd.read_html(r.text)
 
-	    	    df = load_data()
+		    	    df = load_data()
 
-	    	    length = len(df)
-		    
-	    	    if length == 1:
-	    	        st.write("Questa Pagina Web Contiene una sola Pagina Web" )
-	    	    else: st.write("Questa Pagina Web Contiene " + str((length-1)) + " Tabelle" )
-
-
-	    	    st.subheader("") 
-	    	    def createList(r1, r2): 
-	    	        return [item for item in range(r1, r2)] 
-		       
-	    	    r1, r2 = 0, length
-	    	    funct = createList(r1, r2)
-	    	    st.markdown('### **2️⃣ Seleziona la tabella che desideri esportare **')
-	    	    for i in funct:
-		    ###### Selectbox - Selectbox - Selectbox - Selectbox - Selectbox - Selectbox - Selectbox -
-	    	    	with st.expander("Tabella numero : " + str(i)): 
-	    	    		df1 = df[i]
-	    	    		if df1.empty:
-	    	    			st.warning ('ℹ️ - Mannaggia! Qualcosa è andato storto..')
-	    	    		else:
-	    	    			df1 = df1.replace(np.nan, 'empty cell', regex=True)
-	    	    			st.dataframe(df1)
-	    	    			try:
-    	    					rimuoviCredito()
-    	    					nome_web=csv = "web_table_"+str(i)+".csv"
-    	    					csv = df1.to_csv(index=False)
-    	    					b64 = base64.b64encode(csv.encode()).decode()
-    	    					st.markdown('### ** ⬇️ Scarica la tabella in formato csv **')
-    	    					href = f'<a href="data:file/csv;base64,{b64}" download="web_table.csv">** Clicca Qui per Scaricare il Tuo Dataset! 🎉**</a>'
-    	    					st.markdown(href, unsafe_allow_html=True)
+		    	    length = len(df)
+			    
+		    	    if length == 1:
+		    	        st.write("Questa Pagina Web Contiene una sola Pagina Web" )
+		    	    else: st.write("Questa Pagina Web Contiene " + str((length-1)) + " Tabelle" )
 
 
-	    	    			except ValueError:
-	    	    				pass
-	    	    #ValueSelected = st.selectbox('', funct)
-	    	    #st.write('Hai selezionato la Tabella #', ValueSelected)
-	    	    
-		        
-		    #df.columns = df.columns.str.replace(r"[()]", "_
-		    #df2 = df1.val.replace({'vte':'test'}, regex=True)
+		    	    st.subheader("") 
+		    	    def createList(r1, r2): 
+		    	        return [item for item in range(r1, r2)] 
+			       
+		    	    r1, r2 = 0, length
+		    	    funct = createList(r1, r2)
+		    	    st.markdown('### **2️⃣ Seleziona la tabella che desideri esportare **')
+		    	    for i in funct:
+			    ###### Selectbox - Selectbox - Selectbox - Selectbox - Selectbox - Selectbox - Selectbox -
+		    	    	with st.expander("Tabella numero : " + str(i)): 
+		    	    		df1 = df[i]
+		    	    		if df1.empty:
+		    	    			st.warning ('ℹ️ - Mannaggia! Qualcosa è andato storto..')
+		    	    		else:
+		    	    			df1 = df1.replace(np.nan, 'empty cell', regex=True)
+		    	    			st.dataframe(df1)
+		    	    			try:
+	    	    					rimuoviCredito()
+	    	    					nome_web=csv = "web_table_"+str(i)+".csv"
+	    	    					csv = df1.to_csv(index=False)
+	    	    					b64 = base64.b64encode(csv.encode()).decode()
+	    	    					st.markdown('### ** ⬇️ Scarica la tabella in formato csv **')
+	    	    					href = f'<a href="data:file/csv;base64,{b64}" download="web_table.csv">** Clicca Qui per Scaricare il Tuo Dataset! 🎉**</a>'
+	    	    					st.markdown(href, unsafe_allow_html=True)
+
+
+		    	    			except ValueError:
+		    	    				pass
+		    	    #ValueSelected = st.selectbox('', funct)
+		    	    #st.write('Hai selezionato la Tabella #', ValueSelected)
+		    	    
+				
+			    #df.columns = df.columns.str.replace(r"[()]", "_
+			    #df2 = df1.val.replace({'vte':'test'}, regex=True)
+		    else:
+				st.error('Attenzione hai finito i crediti')
 	    	else:
 	    		st.error ("⚠️ - L'URL deve avere un formato valido, Devi iniziare con *https://* o *http://*")    
 	    else:
@@ -365,30 +391,32 @@ def pdftocsv():
 	uploaded_file = st.file_uploader('Scegli un file con estensione .pdf contenente almeno una tabella', type="pdf")
 	if uploaded_file is not None:
 		if st.button("Fammi vedere Che riesci a fare.. - Costo 1 credito"):
-			rimuoviCredito()
-			try:
-				#df = read_pdf(uploaded_file, pages='all')[0]
-				tables = tabula.read_pdf(uploaded_file, pages='all')
-				j=0
-				for tabelle in tables :
-					try:
-						if not tabelle.empty :
-							j=j+1
-							print(tabelle)
-							with st.expander("Tabella numero : " + str(j) ):
-								df_temp = pd.DataFrame(tabelle)
-								df_temp = df_temp.dropna()
-								st.write(df_temp)
-								csv = df_temp.to_csv(index=False)
-								b64 = base64.b64encode(csv.encode()).decode()
-								st.markdown('### ** ⬇️ Scarica la tabella in formato csv **')
-								href = f'<a href="data:file/csv;base64,{b64}" download="PDF_table{str(j)}.csv">** Clicca Qui per Scaricare il Tuo Dataset! 🎉**</a>'
-								st.markdown(href, unsafe_allow_html=True)
-					except ValueError:
-						pass
-			except ValueError:
-				st.info ("ℹ️ - Non abbiamo trovato tabelle da Esportare ! 😊")
-
+			if finecredito() :
+				rimuoviCredito()
+				try:
+					#df = read_pdf(uploaded_file, pages='all')[0]
+					tables = tabula.read_pdf(uploaded_file, pages='all')
+					j=0
+					for tabelle in tables :
+						try:
+							if not tabelle.empty :
+								j=j+1
+								print(tabelle)
+								with st.expander("Tabella numero : " + str(j) ):
+									df_temp = pd.DataFrame(tabelle)
+									df_temp = df_temp.dropna()
+									st.write(df_temp)
+									csv = df_temp.to_csv(index=False)
+									b64 = base64.b64encode(csv.encode()).decode()
+									st.markdown('### ** ⬇️ Scarica la tabella in formato csv **')
+									href = f'<a href="data:file/csv;base64,{b64}" download="PDF_table{str(j)}.csv">** Clicca Qui per Scaricare il Tuo Dataset! 🎉**</a>'
+									st.markdown(href, unsafe_allow_html=True)
+						except ValueError:
+							pass
+				except ValueError:
+					st.info ("ℹ️ - Non abbiamo trovato tabelle da Esportare ! 😊")
+			else:
+				st.error('Attenzione hai finito i crediti')
 
 #################MAIN
 
